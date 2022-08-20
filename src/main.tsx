841f8e9c126b3650c8deff88b966992a8b324111
brunosoares99 <bruno.soares@tindin.com.br>
2022-08-20 17:43:25 -0300

    feat: initial pages structure and home page init

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AppRoute from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <AppRoute />
   </React.StrictMode>
 )
